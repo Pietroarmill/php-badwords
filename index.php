8627f18b642cfbcf548f67f46258ea5c973bc486
Pietroarmill <pietroarmillotta38@gmail.com>
2022-06-07 17:58:18 +0200

    add badword

--- index.php
+++ index.php
@@ -7,11 +7,36 @@
   <title>Document</title>
 </head>
 <body>
-  
 
   <?php
-
+  $bad_Word = $_GET["badword"];
+  $lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla excepturi voluptatibus, velit totam autem aliquam consequuntur explicabo obcaecati saepe adipisci eveniet illum cupiditate quaerat! Accusamus voluptate quam earum doloremque nobis!";
   ?>
+  
+  <p>
+    <?php
+    echo $lorem;
+    ?>
+  </p>
+
+  <span>
+    <?php 
+    echo strlen($lorem)
+    ?>
+  </span>
+
+  <p>
+    <?php
+    $new_lorem = str_replace($bad_Word, "***", $lorem);
+    echo $new_lorem
+    ?>
+  </p>
+
+  <span>
+    <?php 
+    echo strlen($new_lorem)
+    ?>
+  </span>
 
 </body>
 </html>
